3fc2423210d3eeeb8e8bfd109fc808aeff28e235
Franklin Waller <franklin.dijks@gmail.com>
2014-07-05 11:26:48 +0200

    Chat bubbles made more pretty

--- js/phase.js
+++ js/phase.js
@@ -6,10 +6,26 @@
 
 
 function onDocumentReady(){
+    sideMenuOpener();
+}
+
+function sideMenuOpener(){
     var sideMenuOpen = document.getElementsByClassName('sidemenu-open')[0];
     var sideMenuDOM = document.getElementsByClassName('sidemenu')[0];
-  
-    sideMenuOpen.addEventListener("click",function(){
+    var htmlDOM = document.getElementsByTagName('html')[0];
+    var sideMenuSize = window.matchMedia("(max-width: 768px)");
+    
+    if(sideMenuSize.matches){
+        sideMenuOpen.addEventListener("click",function(event){
+            event.preventDefault();
+            event.stopPropagation();
+            sideMenuDOM.style.display = "block";
+        });
+
+        htmlDOM.addEventListener("click",function(event){
+           sideMenuDOM.style.display = "none"; 
+        });
+    }else{
         sideMenuDOM.style.display = "block";
-    });
+    }
 }
